867210a9e0715dea40dba3b44af48ded3dde5212
David Cabiness <dcabiness@werktek.com>
2021-12-03 16:16:28 -0500

    building now

--- src/app/reducers/tutorial.reducer.ts
+++ src/app/reducers/tutorial.reducer.ts
@@ -1,18 +1,30 @@
-import { Action } from "@ngrx/store";
 import { Tutorial } from "../models/tutorial.model";
 import * as TutorialActions from "../actions/tutorial.actions";
+import { createReducer, on } from "@ngrx/store";
 
-const initialState: Tutorial = {
-  name: 'Initial Tutorial',
-  url: 'http://google.com'
+export interface TutorialState {
+  tutorials: Tutorial[];
+}
+
+const initialState: TutorialState = {
+  tutorials: [
+    {
+      name: 'Initial Tutorial',
+      url: 'http://google.com'
+    }
+  ]
 };
 
-export function reducer(state: Tutorial[] = [initialState], action: TutorialActions.Actions) {
-
-  switch(action.type) {
-      case TutorialActions.ADD_TUTORIAL:
-          return [...state, action.payload];
-      default:
-          return state;
-  }
-}
+export const reducer = createReducer(
+  initialState,
+  on(
+    TutorialActions.AddTutorial,
+    (state, { tutorial }) => ({
+      ...state,
+      tutorials: [
+        ...state.tutorials,
+        tutorial
+      ]
+    })
+  )
+);
